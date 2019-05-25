06fb6ed1e2b97cb68ab77ba2a692b74aeea69976
Fletcher Morris <odmoandtheboy@gmail.com>
2019-05-25 23:30:08 +0100

    Application now locates '.component' files from 'components' directory.

--- EngineCompiler/source/EngineCompiler.cpp
+++ EngineCompiler/source/EngineCompiler.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <filesystem>
+#include <direct.h>
+#include <vector>
+#include <string>
+
+
+std::vector<std::string> foundComponentPaths;
+
+void FindComponentFiles()
+{
+	foundComponentPaths = std::vector<std::string>();
+	const std::string c = ".component";
+	for (const auto & entry : std::filesystem::recursive_directory_iterator("components"))
+	{
+		const std::string tempPath = entry.path().u8string();
+		if (tempPath.compare(tempPath.size() - c.size(), c.size(), c) == 0)
+		{
+			foundComponentPaths.push_back((std::string)_getcwd(NULL, 0) + "\\" + entry.path().u8string());
+		}
+	}
+	for (const std::string & path : foundComponentPaths)
+	{
+		std::cout << path << std::endl;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	FindComponentFiles();
+
+	while (1 != 0) {}
 }
