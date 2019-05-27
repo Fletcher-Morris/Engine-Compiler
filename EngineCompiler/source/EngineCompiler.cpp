bff2986a95ef78963fd0fb234683340f0d359560
Fletcher Morris <odmoandtheboy@gmail.com>
2019-05-27 23:34:54 +0100

    Component names are now auto-added to components.cpp file
    Also added 'intermediate' folder, for storing cpp and h files.

--- EngineCompiler/source/EngineCompiler.cpp
+++ EngineCompiler/source/EngineCompiler.cpp
@@ -8,10 +8,12 @@
 
 std::vector<std::string> foundComponentPaths;
 std::vector<std::string> cppPaths;
+std::vector<std::string> componentNames;
 std::string applicationPath;
 const std::string dotComponent = ".component";
 const std::string dotCpp = ".cpp";
 std::string componentsDirectory;
+std::string intermediateDirectory;
 std::string devCommandPromptDir;
 
 void FindComponentFiles()
@@ -19,6 +21,7 @@ void FindComponentFiles()
 	foundComponentPaths = std::vector<std::string>();
 	applicationPath = (std::string)_getcwd(NULL, 0);
 	componentsDirectory = applicationPath + "\\components";
+	intermediateDirectory = componentsDirectory + "\\intermediate";
 	for (const auto & entry : std::filesystem::recursive_directory_iterator("components"))
 	{
 		const std::string tempPath = entry.path().u8string();
@@ -36,8 +39,16 @@ void FindComponentFiles()
 void ConvertCononentToCpp()
 {
 	cppPaths = std::vector<std::string>();
+	componentNames = std::vector<std::string>();
 	for (const std::string & path : foundComponentPaths)
 	{
+		componentNames.push_back(path.substr(componentsDirectory.size() + 1, (path.size() - componentsDirectory.size() - dotComponent.size() - 1)));
+	}
+	for (int i = 0; i < componentNames.size(); i++)
+	{
+		std::string name = componentNames[i];
+		std::string path = foundComponentPaths[i];
+
 		std::ifstream input(path.c_str());
 		if (!input)
 		{
@@ -53,7 +64,6 @@ void ConvertCononentToCpp()
 			bool writingImplem = false;
 			std::string tempLine;
 			bool headerIncluded = false;
-			std::string componentName = path.substr(componentsDirectory.size() + 1, (path.size() - componentsDirectory.size() - dotComponent.size() - 1));
 			while (std::getline(input, tempLine))
 			{
 				if (tempLine.size() > 0)
@@ -68,7 +78,7 @@ void ConvertCononentToCpp()
 						}
 						if (writingImplem)
 						{
-							if (strcmp(tempLine.c_str(), ("#include \"" + componentName + ".h\"").c_str()) == 0) { headerIncluded = true; }
+							if (strcmp(tempLine.c_str(), ("#include \"" + name + ".h\"").c_str()) == 0) { headerIncluded = true; }
 
 							tempImplem.push_back(tempLine);
 						}
@@ -77,7 +87,7 @@ void ConvertCononentToCpp()
 			}
 			if (headerIncluded == false)
 			{
-				implem.push_back("#include \"" + componentName + ".h\"");
+				implem.push_back("#include \"" + name + ".h\"");
 			}
 			for (const std::string & line : tempHeader)
 			{
@@ -88,23 +98,51 @@ void ConvertCononentToCpp()
 				implem.push_back(line);
 			}
 
+			std::filesystem::create_directory(intermediateDirectory);
 			std::ofstream output;
-			output.open(componentsDirectory + "/" + componentName + ".h");
+			output.open(intermediateDirectory + "/" + name + ".h");
 			for (const std::string & line : header)
 			{
 				output << line + "\n";
 			}
 			output.close();
-			output.open(componentsDirectory + "/" + componentName + ".cpp");
+			output.open(intermediateDirectory + "/" + name + ".cpp");
 			for (const std::string & line : implem)
 			{
 				output << line + "\n";
 			}
 			output.close();
 
-			cppPaths.push_back(componentsDirectory + "/" + componentName + ".cpp");
+			cppPaths.push_back(intermediateDirectory + "/" + name + ".cpp");
 		}
 	}
+}
+
+void CreateComponentsClass()
+{
+	std::ofstream output;
+	output.open(intermediateDirectory + "\\components.h");
+	output << "#include <string>\n";
+	for (const std::string & name : componentNames)
+	{
+		output << "#include \"" + name + ".h\"\n";
+	}
+	output << "int GetComponentByName(std::string _componentName);\n";
+	output.close();
+
+	output.open(intermediateDirectory + "\\components.cpp");
+	output << "#include \"components.h\"\n";
+	output << "int GetComponentByName(std::string _componentName)\n";
+	output << "{\n";
+	for (int i = 0; i < componentNames.size(); i++)
+	{
+		output << "if (_componentName == \"" + componentNames[i] + "\") { return " + std::to_string(i) + "; }\n";
+	}
+	output << "return -1;\n";
+	output << "}\n";
+	output.close();
+
+	cppPaths.push_back(intermediateDirectory + "\\components.cpp");
 }
 
 void FindDevCommandPrmopt()
@@ -131,13 +169,13 @@ void CompileFilesToApplication()
 	std::string cppNames = "";
 	for (const std::string & path : cppPaths)
 	{
-		cppNames += (" " + path.substr(componentsDirectory.size() + 1));
+		cppNames += (" " + path.substr(intermediateDirectory.size() + 1));
 	}
 	if (cppPaths.size() >= 1)
 	{
 		std::string cPath = cppPaths[0];
 		std::string actionString = ("c: && cd " + devCommandPromptDir + " && vcvars64 && ");
-		actionString = (actionString + cPath.substr(0, 2) + " && cd " + componentsDirectory + " && cl /EHsc" + cppNames);
+		actionString = (actionString + cPath.substr(0, 2) + " && cd " + intermediateDirectory + " && cl /EHsc" + cppNames + " /link /out:../Program.exe");
 		system(actionString.c_str());
 	}
 }
@@ -147,6 +185,7 @@ int main()
 	FindComponentFiles();
 	FindDevCommandPrmopt();
 	ConvertCononentToCpp();
+	CreateComponentsClass();
 	CompileFilesToApplication();
 
 	while (1 != 0) {}
